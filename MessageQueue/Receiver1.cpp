2e37caec8f6629b1971abf4d7e661db92fc5b636
Daniel <danielkimstudent@hotmail.com>
2018-02-23 00:01:26 -0800

    Finish Receiver1

--- MessageQueue/Receiver1.cpp
+++ MessageQueue/Receiver1.cpp
@@ -21,44 +21,87 @@ using namespace std;
 
 //Constants that define the Senders in which this Receiver listens to
 const int ACCEPT_FROM_1 = 251;
-const int ACCEPT_FROM_2 = 997
+const int ACCEPT_FROM_2 = 997;
 //Defines the size of the character array to be received
 const int MESSAGE_SIZE = 50;
+//Defines the maximum amount of messages that this Receiver could receive
+const int MAX_MESSAGES = 5000;
+//Defines the message type in which this message could receive
+const int RECEIVABLE_MESSAGE_TYPE = 0;
+//Defines the message flag stating that the first message on the queue is received
+const int MESSAGE_FLAG = 0;
 //Variable that keeps track of total received messages
 int receivedMessagesCount = 0;
-
+//Message buffer
+struct buffer {
+	long mtype;
+	long senderId;
+	char message[MESSAGE_SIZE];
+};
 //Forward declaring methods
 int allocateQueue();
+int retrieveMessageSize(buffer);
+void receiveMessage(int, buffer, int, long, int);
 
 /*
 * The main starting point of the application
 */
 int main() {
 
-	int qid = allocateQueue();
+	int messageQueueId = allocateQueue();
 
-	//Message buffer
-	struct buf {
-		long mtype;
-		char message[MESSAGE_SIZE];
-	};
+	buffer message;
+	int messageSize = retrieveMessageSize(message);
 
-	buf msg;
-	int size = sizeof(msg)-sizeof(long);
+	receiveMessage(
+		messageQueueId,
+		message,
+		messageSize,
+		RECEIVABLE_MESSAGE_TYPE,
+		MESSAGE_FLAG
+	);
 
 	exit(0);
 }
 
 /*
-*	Allocates a queue where message objects are saved
+*	Wrapper method that allocates and creates a queue where message objects are
+* saved
 *
 * @Param: None
 *
 *	@Return: A message queue identifier
 */
 int allocateQueue() {
-	string filePath = ".";
 	int id = 'u';
+	return msgget(ftok(".", id), IPC_EXCL|IPC_CREAT|0777);
+}
 
-	return msgget(ftok(filePath, id), IPC_EXCL|IPC_CREAT|0777);
+/*
+* Wrapper method that outputs the size of the message to be received
+*
+*	@Param:
+*	buffer message: The message buffer to be received
+*
+* @Return: The size of the message to be received
+*/
+int retrieveMessageSize(buffer message) {
+	return sizeof(message) - sizeof(long) - sizeof(long);
+}
+
+/*
+* Receives a message specifically through the given arguments
+*
+*@Param:
+*int queueId: The message queue id
+*buffer message: The message buffer object received
+*int messageSize: The size of the message to be received
+*long messageType: The message type specified by the received object
+*int messageFlag: The flag that specifies the type of the message requested
+*
+*@Return: None
+*
+*/
+void receiveMessage(int queueId, buffer message, int messageSize, long messageType, int messageFlag) {
+	msgrccv(queueId, (struct buffer *)&message, messageSize, messageType, messageFlag);
 }
