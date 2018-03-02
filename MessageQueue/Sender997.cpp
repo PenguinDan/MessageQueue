1cabd0db58b3c1dd9395deb1b4884926b4c91740
Mario <mario.lopezherdandez@student.csulb.edu>
2018-03-01 21:49:33 -0800

    commented out sender 2

--- MessageQueue/Sender997.cpp
+++ MessageQueue/Sender997.cpp
@@ -34,30 +34,37 @@ int main(){
     };buf msg;
     initializeSRand();
 
+    int messageSize = retrieveMessageSize(msg);
     msg.mtype = 0;
     msg.senderID = SENDER_ID;
     strcpy(msg.message, "Sender 997 to Reciver 1");
     cout<< " Message Sent to Reciever 1" <<endl;
-    msgsnd(qid, (struct msgbuf *)&msg, MESSAGE_SIZE, 0);
+    msgsnd(qid, (struct msgbuf *)&msg, messageSize, 0);
 
+    /*
     msg.mtype = 1;
     msg.senderID = SENDER_ID;
     strcpy(msg.message, "Sender 997 to Reciver 2");
     cout<< " Message Sent to Reciever 2" <<endl;
-    msgsnd(qid, (struct msgbuf *)&msg, MESSAGE_SIZE, 0);
+    msgsnd(qid, (struct msgbuf *)&msg, messageSize, 0);
+    */
 
-    msgrcv(qid, (struct msgbuf *)&msg, MESSAGE_SIZE, 2, 0);
+    msgrcv(qid, (struct msgbuf *)&msg, messageSize, 2, 0);
   	cout << "Response Recived 1" << endl;
   	cout << "reply: " << msg.message << endl;
   	cout << ": now exits" << endl;
 
-    msgrcv(qid, (struct msgbuf *)&msg, MESSAGE_SIZE, 2, 0);
+    /*
+    msgrcv(qid, (struct msgbuf *)&msg, messageSize, 2, 0);
   	cout << "Response Recived 2" << endl;
   	cout << "reply: " << msg.message << endl;
   	cout << ": now exits" << endl;
+    */
 
 }
-
+int retrieveMessageSize(buffer message) {
+    return sizeof(message) - sizeof(long) - sizeof(long);
+}
 //Generate a random number
 int generateRandomNum(){
     return rand() % INT_MAX;
