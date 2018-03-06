7d5fe5d8c59b8c3703332af93d7b7eb6b71983e8
PenguinDan <danielkimstudent@hotmail.com>
2018-03-05 23:36:15 -0800

    Make Receiver1 Pretty

--- MessageQueue/Receiver1.cpp
+++ MessageQueue/Receiver1.cpp
@@ -27,13 +27,13 @@ const int MESSAGE_SIZE = 50;
 //Defines the maximum amount of messages that this Receiver could receive
 const int MAX_MESSAGES = 5000;
 //Defines the message type in which this message could receive
-const int RECEIVABLE_MESSAGE_TYPE = 0;
+const int RECEIVABLE_MESSAGE_TYPE = 1;
 //Defines the message flag stating that the first message on the queue is received
 const int MESSAGE_FLAG = 0;
 //Message buffer
 struct buffer {
-    long mType;
-    // long senderId;
+    long messageType;
+    long senderId;
     char message[MESSAGE_SIZE];
 };
 //Forward declaring methods
@@ -47,48 +47,62 @@ void receiveMessage(int, buffer, int, long, int);
  */
 int main() {
 
-    //The Queue ID of the message queue
+    //The Message Queue ID that other programs connect to
     int messageQueueId = allocateQueue();
+    //Message that prints out the message queue ID for easy deallocation
+    //from the terminal
     cout << "Create Message Queue ID: " << messageQueueId << endl;
-
     //Build buffer for message acknowledgement to sender 997
-    buffer ackMessage;
-    ackMessage.mType = 2;
-    // ackMessage.senderId = 997;
-    strcpy(ackMessage.message, "Acknowledgement from Receiver 1");
+    //Sender 997 only receives messages of messageType 2
+    buffer acknowledgementMessage;
+    acknowledgementMessage.messageType = 2;
+    acknowledgementMessage.senderId = 1;
+    strcpy(acknowledgementMessage.message, "Acknowledgement from Receiver 1");
+    //Create a buffer object to store the received messages
+    buffer receivedMessage;
+    int messageSize = retrieveMessageSize(receivedMessage);
 
-    buffer rMessage;
-    int messageSize = retrieveMessageSize(rMessage);
-
-    //Variable that keeps track of total received messages
+    //Variable that keeps track of total received messages, once it reaches
+    //4999, it will force the loop to exit and terminate the message queue
     int receivedMessagesCount = 0;
 
-    // while(receivedMessagesCount < 5000) {
+    cout << "Entering while loop" << endl;
+
+    while(receivedMessagesCount < 5000) {
+        //Waits until a message with the proper message type specified by
+        //the constant RECEIVABLE_MESSAGE_TYPE is the current first message
+        //in the message queue
         receiveMessage(
                        messageQueueId,
-                       rMessage,
+                       receivedMessage,
                        messageSize,
                        RECEIVABLE_MESSAGE_TYPE,
                        MESSAGE_FLAG
                        );
-
-        //Print out the received message
-        cout << rMessage.message << endl;
+        //The message has been received, specify which sender the message
+        //was received from and the message
+        cout << "Message from Sender #" << receivedMessage.senderId <<
+          ": " << rMessage.message << endl;
+        //Increment the message received counter by one
         receivedMessagesCount += 1;
-
-        // if(rMessage.senderId == ackMessage.senderId) {
+        //Check if the message was sent from Sender #997
+        if(receivedMessage.senderId == acknowledgementMessage.senderId) {
+            //The message was received from Sender #997
             cout << "Sending acknowledgement message to Sender 997" << endl;
-            sendMessageAcknowledgement(messageQueueId, ackMessage, messageSize, MESSAGE_FLAG);
-        // }
-    // }
-
+            //Send the acknowledgement message to Sender 997
+            sendMessageAcknowledgement(messageQueueId, acknowledgementMessage,
+              messageSize, MESSAGE_FLAG);
+        }
+    }
+    //This receiver has received 5000 messages, begin closing the queue
     cout << "Closing message queue" << endl;
     msgctl(messageQueueId, IPC_RMID, NULL);
+    //Exit the program
     exit(0);
 }
 
 /*
- *    Wrapper method that allocates and creates a queue where message objects are
+ * Wrapper method that allocates and creates a queue where message objects are
  * saved
  *
  * @Param: None
@@ -103,24 +117,25 @@ int allocateQueue() {
 /*
  * Wrapper method that outputs the size of the message to be received
  *
- *    @Param:
+ * @Param:
  *    buffer message: The message buffer to be received
  *
- * @Return: The size of the message to be received
+ * @Return:
+ *    The size of the message to be received
  */
 int retrieveMessageSize(buffer message) {
-    return sizeof(message) - sizeof(long);
+    return sizeof(message) - sizeof(long) - sizeof(long);
 }
 
 /*
  * Receives a message specifically through the given arguments
  *
  *@Param:
- *int queueId: The message queue id
- *buffer message: The message buffer object received
- *int messageSize: The size of the message to be received
- *long messageType: The message type specified by the received object
- *int messageFlag: The flag that specifies the type of the message requested
+ *  int queueId: The message queue id
+ *  buffer message: The message buffer object received
+ *  int messageSize: The size of the message to be received
+ *  long messageType: The message type specified by the received object
+ *  int messageFlag: The flag that specifies the type of the message requested
  *
  *@Return: None
  *
